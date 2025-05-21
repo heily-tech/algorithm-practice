3f15c6204e8fc977a2df6fa9f609fe4415201f97
heily-tech <edelbteen@gmail.com>
2025-05-21 12:55:59 +0900

    refactor: extract group word logic in BOJ 1316 to isGroupWord method

--- src/main/java/org/example/string/BOJ_1316.java
+++ src/main/java/org/example/string/BOJ_1316.java
@@ -7,8 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 /*
- * 메모리 14264KB
- * 시간 104ms
+ * 메모리 14284KB
+ * 시간 100ms
  */
 
 public class BOJ_1316 {
@@ -19,33 +19,27 @@ public class BOJ_1316 {
         int groupCount = 0;
 
         for (int i = 0; i < N; i++) {
-            Set<Character> visited = new HashSet<>();
-            boolean isGroupWord = true;
-
             String word = br.readLine();
-            
-            // 2. 그룹 단어 판단
-            char prev = 0;
-            for (int j = 0; j < word.length(); j++) {
-                char curr = word.charAt(j);
-
-                if (curr != prev) {
-                    if (visited.contains(curr)) { // 이전 문자와 다르고, 등장한 적이 있음
-                        isGroupWord = false;
-                        break;
-                    }
-                    // 이전 문자와 다르고, 등장한 적이 없음
-                    visited.add(curr);
-                }
-                prev = curr;
-            }
-
-            // 3. 그룹 단어 개수 저장
-            if (isGroupWord)
+            if (isGroupWord(word))
                 groupCount++;
         }
 
-        // 4. 결과 출력
         System.out.println(groupCount);
     }
+
+    private static boolean isGroupWord(String word) {
+        boolean[] alphabet = new boolean[26];
+        char prev = 0;
+
+        for (char curr : word.toCharArray()) {
+            if (curr != prev) {
+                if (alphabet[curr - 'a'])
+                    return false;
+                alphabet[curr - 'a'] = true;
+            }
+            prev = curr;
+        }
+        return true;
+    }
+
 }
