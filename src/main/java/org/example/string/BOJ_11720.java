70b1b5ba4a6e6d3917235afda922bb051ddad25c
heily-tech <edelbteen@gmail.com>
2025-05-21 12:01:10 +0900

    feat: solve BOJ 11720 (숫자의 합)
    - read digits as a string and summed each character as integer

---
+++ src/main/java/org/example/string/BOJ_11720.java
@@ -0,0 +1,28 @@
+package org.example.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * 메모리 14192KB
+ * 시간 100ms
+ */
+
+public class BOJ_11720 {
+    public static void main(String[] args) throws IOException {
+        // 1. 입력 받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int sum = 0;
+
+        // 2. 문자 합하기
+        for (int i = 0; i < N; i++) {
+            sum += Integer.parseInt(String.valueOf(s.charAt(i)));
+        }
+
+        // 3. 숫자들의 합 출력하기
+        System.out.println(sum);
+    }
+}
