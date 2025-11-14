d4d8faf5eccf517b7fa027d33622bff14125a935
heily-tech <90965441+heily-tech@users.noreply.github.com>
2025-11-14 17:06:20 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 1.27 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -4,31 +4,28 @@ class Solution {
         int curr = toSec(pos);
         int opStart = toSec(op_start);
         int opEnd = toSec(op_end);
-        
+
         curr = jump(curr, opStart, opEnd);
-        
+
         for (String com : commands) {
-            if (com.equals("next"))
-                curr += 10;
-            else
-                curr -= 10;
-        
-            if (curr < 0)
-                curr = 0;
-            if (curr > video)
-                curr = video;
-            
-            curr = jump(curr, opStart, opEnd);
+            curr += com.equals("next") ? 10 : -10; 
+
+            curr = clamp(curr, 0, video);         
+            curr = jump(curr, opStart, opEnd);    
         }
-        
+
         return String.format("%02d:%02d", curr / 60, curr % 60);
     }
-    
-    private int toSec(String time) {
-        String[] times = time.split(":");
-        return Integer.parseInt(times[0]) * 60 + Integer.parseInt(times[1]);
+
+    private int toSec(String t) {
+        String[] s = t.split(":");
+        return Integer.parseInt(s[0]) * 60 + Integer.parseInt(s[1]);
     }
-    
+
+    private int clamp(int x, int min, int max) {
+        return Math.max(min, Math.min(max, x));
+    }
+
     private int jump(int pos, int start, int end) {
         return (pos >= start && pos <= end) ? end : pos;
     }
