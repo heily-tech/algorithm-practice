402d442b5b00989f87ee9143daf5eb315be0468f
heily-tech <90965441+heily-tech@users.noreply.github.com>
2026-01-06 18:54:43 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 13.78 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,8 +1,5 @@
 class Solution {
     public String solution(String s) {
-        if (s == null || s.trim().isEmpty())
-            throw new IllegalArgumentException("입력이 비어 있습니다.");
-
         String[] strs = s.trim().split("\\s+");
 
         int min = Integer.MAX_VALUE;
@@ -10,16 +7,10 @@ class Solution {
 
         for (String str : strs) {
             int num;
-            try {
-                num = Integer.parseInt(str);
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다: " + str);
-            }
-
-            if (num < min)
-                min = num;
-            if (num > max)
-                max = num;
+            num = Integer.parseInt(str);
+            
+            min = Math.min(min, num);
+            max = Math.max(max, num);
         }
 
         return min + " " + max;
