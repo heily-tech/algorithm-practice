6ce046bf7efd07f3d1b3512e14ea48675022a48a
heily-tech <90965441+heily-tech@users.noreply.github.com>
2026-01-08 16:47:14 +0900

    [Silver IV] Title: 베스트셀러, Time: 108 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Map<String, Integer> bookSell = new HashMap(N);
+
+        for (int i = 0; i < N; i++) {
+            String key = br.readLine();
+            bookSell.put(key, bookSell.getOrDefault(key, 0) + 1);
+        }
+
+        String bestSeller = null;
+        int max = 0;
+
+        for (Map.Entry<String, Integer> entry : bookSell.entrySet()) {
+            if (max < entry.getValue() || (entry.getValue() == max && (bestSeller == null || entry.getKey().compareTo(bestSeller) < 0))) {
+                max = entry.getValue();
+                bestSeller = entry.getKey();
+            }
+        }
+
+        System.out.println(bestSeller);
+    }
+}
