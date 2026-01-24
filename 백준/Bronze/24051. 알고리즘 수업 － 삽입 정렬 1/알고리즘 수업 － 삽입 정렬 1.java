b4047d95a3408751712c74cb461b209af72d4c4d
heily-tech <90965441+heily-tech@users.noreply.github.com>
2026-01-24 22:25:11 +0900

    [Bronze I] Title: 알고리즘 수업 - 삽입 정렬 1, Time: 212 ms, Memory: 16388 KB -BaekjoonHub

---
+++ 백준/Bronze/24051. 알고리즘 수업 － 삽입 정렬 1/알고리즘 수업 － 삽입 정렬 1.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        insertionSort(arr, K);
+    }
+
+    private static void insertionSort(int[] arr, int K) {
+        int cnt = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            int newItem = arr[i];
+            int curr = i - 1;
+
+            while (curr >= 0 && newItem < arr[curr]) {
+                arr[curr + 1] = arr[curr];
+                if (++cnt == K) {
+                    System.out.println(arr[curr + 1]);
+                    return;
+                }
+                curr--;
+            }
+
+            if (curr + 1 != i) {
+                arr[curr + 1] = newItem;
+                if (++cnt == K) {
+                    System.out.println(newItem);
+                    return;
+                }
+            }
+        }
+
+        System.out.println(-1);
+    }
+}
