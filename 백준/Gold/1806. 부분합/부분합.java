05a36047f78b711bcedd06a255a00d34f491b446
heily-tech <90965441+heily-tech@users.noreply.github.com>
2026-01-29 23:36:45 +0900

    [Gold IV] Title: 부분합, Time: 260 ms, Memory: 23840 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        int left = 0;
+        long sum = 0;
+        int ans = n + 1;
+
+        for (int right = 0; right < n; right++) {
+            sum += arr[right];
+
+            while (sum >= s) {
+                ans = Math.min(ans, right - left + 1);
+                sum -= arr[left];
+                left++;
+            }
+        }
+
+        System.out.println(ans == n + 1 ? 0 : ans);
+    }
+}
