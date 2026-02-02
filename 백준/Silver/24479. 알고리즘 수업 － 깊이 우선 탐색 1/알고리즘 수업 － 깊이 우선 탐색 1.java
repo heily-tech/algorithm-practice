78dca61fe7041d58a4418f2c9b000b0b77427f4e
heily-tech <90965441+heily-tech@users.noreply.github.com>
2026-02-02 16:28:38 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 776 ms, Memory: 88396 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Main {
+    private static List<Integer>[] graph;
+    private static int[] visitOrder;
+    private static int order = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        for (int i = 1; i <= n; i++) Collections.sort(graph[i]);
+
+        visitOrder = new int[n + 1];
+        dfs(r);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) sb.append(visitOrder[i]).append('\n');
+        System.out.print(sb);
+    }
+
+    private static void dfs(int u) {
+        visitOrder[u] = order++;
+
+        for (int v : graph[u])
+            if (visitOrder[v] == 0)
+                dfs(v);
+
+    }
+
+}
